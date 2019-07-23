511a4608f39fdfd1cac6fa41d6f3aa9122295c41
Ajay Kumar <ajax99k@gmail.com>
2019-07-23 22:01:05 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask , request , jsonify
+from flask_restful import Resource , Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class TimetableApi(Resource):
+    def post(self):
+        req = request.get_json()
+        return {'result' : [req , req]} , 201
+
+api.add_resource(TimetableApi , '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
